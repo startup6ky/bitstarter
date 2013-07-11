84809d01b665af9c39efab67355903f30e8354ab
startup6ky <startup6.ky@gmail.com>
2013-07-11 14:08:45 +0000

    Updated web.js, Added index.html

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
-var app = express.createServer(express.logger());
+var app = express();
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/', function(request, response){
+	// response.send("Hello World!");
+	buffer = fs.readFileSync('index.html');
+	response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(port, function(){
+	console.log("Listening on " + port);
 });
